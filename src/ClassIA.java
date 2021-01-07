16cb9d25fc8e0ee8ed2a21210e8c2f72b5e3c159
Roberto Spotti <roberto.spotti@gmail.com>
2021-01-07 21:23:00 +0100

    Versione con lo switch

--- src/ClassIA.java
+++ src/ClassIA.java
@@ -24,43 +24,34 @@ public class ClassIA {
 		do {
 			risposta=in.nextLine();
 		} while (risposta.isEmpty()); // controllare che la stringa sia vuota o meno
-		
-		if (risposta.equalsIgnoreCase("no") ) {
-			System.out.println("Addio");
-		} else {
-			 if (risposta.equalsIgnoreCase("si")) {
+
+		switch (risposta.toLowerCase()) {
+			case "no":
+				System.out.println("Addio");
+				break;
+			case "si":
 				 System.out.println("dimmi il tuo nome per favore");
 				 do {
 					risposta=in.nextLine();
 				 } while (risposta.isEmpty());
-			 }
-//			
-//			 System.out.println("Hai scritto: "+risposta);
-//			 System.out.println("risposta è lunga: "+risposta.length());
-//			 System.out.println("L'ultimo carattere è "+risposta.charAt(risposta.length()-1));
-			/*
-			 *  cercare di capire se maschio o femmina
-			 *  Se il nome <> Andrea o Tobia => se finisce per a femmina
-			 *  Se il nome termina per 'o' oppure per 'e' maschio 
-			 *  Se termina con altra lettera risponde "ciao ciao".
-			 */
-			   // si continua anche qui
-			   String nome=risposta.toLowerCase().replaceFirst(""+risposta.toLowerCase().charAt(0), ""+risposta.toUpperCase().charAt(0));
-				// O anche: 
-			    //String nome = risposta.substring(0, 1).toUpperCase() + risposta.substring(1).toLowerCase();
-		
-				if ( nome.equalsIgnoreCase("tobia") ||
-					 nome.equalsIgnoreCase("andrea") || 
-					 nome.charAt(nome.length()-1)=='o' ||
-					 nome.charAt(nome.length()-1)=='e') {
-					System.out.println("Buongiorno signor "+nome);
-				} else if ( nome.charAt(nome.length()-1)=='a' ) {
-					System.out.println("Buongiorno signora "+nome);
-				} else {
-					System.out.println("Ciao ciao");
-				}
-		}
-
+			default: // senza il break se esegue il caso 
+				     // si continua anche qui
+				   String nome=risposta.toLowerCase().replaceFirst(""+risposta.toLowerCase().charAt(0), ""+risposta.toUpperCase().charAt(0));
+					// O anche: 
+				    //String nome = risposta.substring(0, 1).toUpperCase() + risposta.substring(1).toLowerCase();
+			
+					if ( nome.equalsIgnoreCase("tobia") ||
+						 nome.equalsIgnoreCase("andrea") || 
+						 nome.charAt(nome.length()-1)=='o' ||
+						 nome.charAt(nome.length()-1)=='e') {
+						System.out.println("Buongiorno signor "+nome);
+					} else if ( nome.charAt(nome.length()-1)=='a' ) {
+						System.out.println("Buongiorno signora "+nome);
+					} else {
+						System.out.println("Ciao ciao");
+					}
+					break;
+		} // case
 	}
 
 }
